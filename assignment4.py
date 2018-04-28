8a317ace27473cf78e58b1e917c2c37ab93c56a8
parisaSS <parisasorkhi113@gmail.com>
2018-04-28 12:49:35 -0700

    taghir dar code

--- assignment4.py
+++ assignment4.py
@@ -1,22 +1,35 @@
-def port_scanner():
-    """
-    write a port scanner using nmap library
-    for scanning 10 arbitrary hosts and and ports between
-    21-1000 then write the result to a file using
-    write_to_file function
-    """
 
-def write_to_file(contents):
-    """
-    first remove the pass from function body
-    then write your code
-    :param contents: the result that should be written to file
-    :return: true if the writing successfull and false if can't
-    """
+import nmap
+
+def port_scanner(ports):
+    nm = nmap.PortScanner()
+    nm.scan(ports, '21-1000')
+    for host_names in nm.all_hosts():
+        ports = nm[host_names].get('tcp')
+        write_into_file(ports)
+
+    return ports
+
+
+def write_into_file(contents):
+    f = open('result', 'w')
+    ff = str(contents)
+    f.write(ff)
+
     pass
 
 
 def main():
-    port_scanner()
+    hosts = [
+        'w3schools.com',
+        'hadipoor.ir'
+        'google.com',
+        'hibibhoora.com',
+        'fileniko.com',
+        'tvniko.com'
+    ]
+
+    for host_name in hosts:
+        port_scanner(host_name)
 
 main()
